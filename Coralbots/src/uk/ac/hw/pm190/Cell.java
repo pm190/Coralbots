9b900f43d2cbd028e3b17dae3dc38c25d58fc048
Patrick Mackinder <pm190@hw.ac.uk>
2014-02-02 17:42:01 +0000

    Added y and z coordinates

--- Coralbots/src/uk/ac/hw/pm190/Cell.java
+++ Coralbots/src/uk/ac/hw/pm190/Cell.java
@@ -3,14 +3,28 @@ package uk.ac.hw.pm190;
 public class Cell 
 {
 	private final int x;
+	private final int y;
+	private final int z;
 	
-	public Cell(int x)
+	public Cell(int x, int y, int z)
 	{
 		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 	
 	public int getX()
 	{
 		return x;
+	}
+
+	public int getY() 
+	{
+		return y;
+	}
+
+	public int getZ() 
+	{
+		return z;
 	}
 }
